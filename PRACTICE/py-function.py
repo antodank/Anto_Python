f15646e2806f6abaa9e98e13ac45338cc027df50
Ankit Todankar <36885958+antodank@users.noreply.github.com>
2025-12-14 20:32:05 +0530

    14 dec 2025

---
+++ PRACTICE/py-function.py
@@ -0,0 +1,15 @@
+def say_hello():
+    print('hello')
+
+def greeting(name):
+    print(f"hello {name}")
+
+say_hello()
+greeting("Ankit")
+
+def add_num(num1,num2):
+    return num1+num2
+
+print(add_num(30,20))
+
+
